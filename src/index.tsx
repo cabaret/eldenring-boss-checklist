63c8f164222d14115a6464eda6d71d0227378c4f
Joris Ooms <hello@cupofcode.be>
2022-05-26 17:44:37 +0200

    add cupofcode thing

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,9 @@ import App from './App'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement)
 root.render(
   <React.StrictMode>
+    <a href="https://cupofcode.be" className="fixed bottom-0 right-0 z-20 bg-black">
+      <img src="https://cupofcode.be/favicon-32x32.png" alt="" />
+    </a>
     <App />
   </React.StrictMode>,
 )
